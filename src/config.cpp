384732aeb913febfb06460a5d65012af72a44c35
arjan <github.com@arjanhouben.nl>
2013-12-07 17:04:29 +0100

    some work done on debug script, needs to be some way of cross-debugger functinality available..

--- src/config.cpp
+++ src/config.cpp
@@ -17,224 +17,234 @@ typedef map< string, string > Values;
 
 path defaultBaseDir()
 {
-	const char *homeDir = getenv( "HOME" );
-	if ( !homeDir )
-	{
-		throw logic_error( "no 'HOME' directory found in environment!" );
-	}
-	return path( homeDir ) / ".nativescript/";
+    const char *homeDir = getenv( "HOME" );
+    if ( !homeDir )
+    {
+        throw logic_error( "no 'HOME' directory found in environment!" );
+    }
+    return path( homeDir ) / ".nativescript/";
+}
+
+void trim( string &s )
+{
+    auto start = s.begin();
+    while ( start != s.end() && isspace( *start ) )
+    {
+        start = s.erase( start );
+    }
 }
 
 Values parseConfig( std::istream &stream )
 {
-	Values values;
+    Values values;
 
-	int lineNr = 0;
-	string tmp;
-	while ( getline( stream, tmp ) )
-	{
-		++lineNr;
+    int lineNr = 0;
+    string tmp;
+    while ( getline( stream, tmp ) )
+    {
+        ++lineNr;
 
-		if ( tmp.empty() ) continue;
+        if ( tmp.empty() ) continue;
 
-		stringstream line( tmp );
+        stringstream line( tmp );
 
-		char singleChar;
-		string key;
-		if ( ( line >> key >> singleChar ) && singleChar == '=' )
-		{
-			values[ key ] = tmp.substr( line.tellg() );
-		}
-		else if ( !key.empty() && key[ 0 ] != '#' )
-		{
-			stringstream tmpstream;
-			tmpstream << "error on config line: " << lineNr << ", line did not follow 'key = value' format";
-			throw logic_error( tmpstream.str() );
-		}
-	}
+        char singleChar;
+        string key;
+        if ( ( line >> key >> singleChar ) && singleChar == '=' )
+        {
+            values[ key ] = tmp.substr( line.tellg() );
+            trim( values[ key ] );
+        }
+        else if ( !key.empty() && key[ 0 ] != '#' )
+        {
+            stringstream tmpstream;
+            tmpstream << "error on config line: " << lineNr << ", line did not follow 'key = value' format";
+            throw logic_error( tmpstream.str() );
+        }
+    }
 
-	return values;
+    return values;
 }
 
 class SaveValue
 {
-	public:
+    public:
 
-		SaveValue( string *destination = 0 ) :
-			destination_( destination ) { }
+        SaveValue( string *destination = 0 ) :
+            destination_( destination ) { }
 
-		bool set( const string &value )
-		{
-			if ( destination_ && !value.empty() )
-			{
-				*destination_ = value;
-			}
-			return destination_;
-		}
+        bool set( const string &value )
+        {
+            if ( destination_ && !value.empty() )
+            {
+                *destination_ = value;
+            }
+            return destination_;
+        }
 
-		std::string str() const
-		{
-			return destination_ ? *destination_ : std::string();
-		}
+        std::string str() const
+        {
+            return destination_ ? *destination_ : std::string();
+        }
 
-	private:
+    private:
 
-		string *destination_;
+        string *destination_;
 };
 
 ostream& operator << ( ostream &stream, const SaveValue &value )
 {
-	return stream << value.str();
+    return stream << value.str();
 }
 
 Config::Config() :
-	baseDirectory_( defaultBaseDir().string() ),
-	buildDirectory_( "build/" ),
-	sourceDirectory_( "source/" ),
-	header_( "header.cpp" ),
-	footer_( "footer.cpp" ),
-	compilerCommand_( detected::compiler ),
-	debugCommand_( "lldb" ),
-	cxxFlags_( detected::cxx_flags ),
-	releaseFlags_( detected::cxx_flags_release ),
-	debugFlags_( detected::cxx_flags_debug ),
-	includeDirectories_(),
-	linkDirectories_(),
-	libraries_()
+    baseDirectory_( defaultBaseDir().string() ),
+    buildDirectory_( "build/" ),
+    sourceDirectory_( "source/" ),
+    header_( "header.cpp" ),
+    footer_( "footer.cpp" ),
+    compilerCommand_( detected::compiler ),
+    debugCommand_( "lldb" ),
+    cxxFlags_( detected::cxx_flags ),
+    releaseFlags_( detected::cxx_flags_release ),
+    debugFlags_( detected::cxx_flags_debug ),
+    includeDirectories_(),
+    linkDirectories_(),
+    libraries_()
 {
 }
 
 map< string, SaveValue > Config::getConfigValues()
 {
-	map<string, SaveValue> saveValues;
-	saveValues[ "base_directory" ] = &baseDirectory_;
-	saveValues[ "build_directory" ] = &buildDirectory_;
-	saveValues[ "source_directory" ] = &sourceDirectory_;
-	saveValues[ "compiler" ] = &compilerCommand_;
-	saveValues[ "debugger" ] = &debugCommand_;
-	saveValues[ "cxx_flags" ] = &cxxFlags_;
-	saveValues[ "release_flags" ] = &releaseFlags_;
-	saveValues[ "debug_flags" ] = &debugFlags_;
-	saveValues[ "include_directories" ] = &includeDirectories_;
-	saveValues[ "link_directories" ] = &linkDirectories_;
-	saveValues[ "libraries" ] = &libraries_;
-	return saveValues;
+    map<string, SaveValue> saveValues;
+    saveValues[ "base_directory" ] = &baseDirectory_;
+    saveValues[ "build_directory" ] = &buildDirectory_;
+    saveValues[ "source_directory" ] = &sourceDirectory_;
+    saveValues[ "compiler" ] = &compilerCommand_;
+    saveValues[ "debugger" ] = &debugCommand_;
+    saveValues[ "cxx_flags" ] = &cxxFlags_;
+    saveValues[ "release_flags" ] = &releaseFlags_;
+    saveValues[ "debug_flags" ] = &debugFlags_;
+    saveValues[ "include_directories" ] = &includeDirectories_;
+    saveValues[ "link_directories" ] = &linkDirectories_;
+    saveValues[ "libraries" ] = &libraries_;
+    return saveValues;
 }
 
 Config Config::readConfigFile( const path &configPath )
 {
-	Config config;
+    Config config;
 
-	ifstream configFile( configPath.c_str() );
-	if ( !configFile )
-	{
-		const path sample( configPath + ".sample" );
-		if ( !exists( sample ) )
-		{
-			make_directory( sample.dirname() );
-			ofstream defaultFile( sample.c_str() );
-			writeDefault( defaultFile );
-		}
-	}
-	else
-	{
-		try
-		{
-			Values values( parseConfig( configFile ) );
+    ifstream configFile( configPath.c_str() );
+    if ( !configFile )
+    {
+        const path sample( configPath + ".sample" );
+        if ( !exists( sample ) )
+        {
+            make_directory( sample.dirname() );
+            ofstream defaultFile( sample.c_str() );
+            writeDefault( defaultFile );
+        }
+    }
+    else
+    {
+        try
+        {
+            Values values( parseConfig( configFile ) );
 
-			map< string, SaveValue > saveValues( config.getConfigValues() );
+            map< string, SaveValue > saveValues( config.getConfigValues() );
 
-			for ( auto value : values )
-			{
-				if ( !saveValues[ value.first ].set( value.second ) )
-				{
-					throw logic_error( "\"" + value.first + "\" is not a known configuration setting" );
-				}
-			}
-		}
-		catch ( const exception &err )
-		{
-			throw logic_error( "error while trying to parse \"" + configPath.string() + "\"\n\"" + err.what() );
-		}
-	}
+            for ( auto value : values )
+            {
+                if ( !saveValues[ value.first ].set( value.second ) )
+                {
+                    throw logic_error( "\"" + value.first + "\" is not a known configuration setting" );
+                }
+            }
+        }
+        catch ( const exception &err )
+        {
+            throw logic_error( "error while trying to parse \"" + configPath.string() + "\"\n\"" + err.what() );
+        }
+    }
 
-	return config;
+    return config;
 }
 
 void Config::writeDefault( ostream &stream )
 {
-	Config config;
+    Config config;
 
-	stream << "# these are the default values, you can change them to your preference" << endl
-		   << "# if this file is missing, it will be auto-generated" << endl
-		   << "# this file needs to be renamed from 'config.sample' to 'config' for it to be effective" << endl << endl;
-	for ( auto i : config.getConfigValues() )
-	{
-		stream << i.first << " = " << i.second << endl << endl;
-	}
+    stream << "# these are the default values, you can change them to your preference" << endl
+           << "# if this file is missing, it will be auto-generated" << endl
+           << "# this file needs to be renamed from 'config.sample' to 'config' for it to be effective" << endl << endl;
+    for ( auto i : config.getConfigValues() )
+    {
+        stream << i.first << " = " << i.second << endl << endl;
+    }
 }
 
 path Config::baseDirectory() const
 {
-	return baseDirectory_;
+    return baseDirectory_;
 }
 
 path Config::buildDirectory() const
 {
-	return absolute( buildDirectory_, baseDirectory() );
+    return absolute( buildDirectory_, baseDirectory() );
 }
 
 path Config::sourceDirectory() const
 {
-	return absolute( sourceDirectory_, baseDirectory() );
+    return absolute( sourceDirectory_, baseDirectory() );
 }
 
 string Config::compilerCommand() const
 {
-	return compilerCommand_;
+    return compilerCommand_;
 }
 
 string Config::debugCommand() const
 {
-	return debugCommand_;
+    return debugCommand_;
 }
 
 string Config::cxxFlags() const
 {
-	return cxxFlags_;
+    return cxxFlags_;
 }
 
 string Config::releaseFlags() const
 {
-	return releaseFlags_;
+    return releaseFlags_;
 }
 
 string Config::debugFlags() const
 {
-	return debugFlags_;
+    return debugFlags_;
 }
 
 string Config::includeDirectories() const
 {
-	return includeDirectories_;
+    return includeDirectories_;
 }
 
 string Config::linkDirectories() const
 {
-	return linkDirectories_;
+    return linkDirectories_;
 }
 
 string Config::libraries() const
 {
-	return libraries_;
+    return libraries_;
 }
 
 string Config::header() const
 {
-	return header_;
+    return header_;
 }
 
 string Config::footer() const
 {
-	return footer_;
+    return footer_;
 }
--- src/nativescript.cpp
+++ src/nativescript.cpp
@@ -18,28 +18,28 @@ void handle_script( int, char *[] );
 
 enum build_type
 {
-	release,
-	debug
+    release,
+    debug
 };
 
 class action
 {
-	public:
+    public:
 
-		action() :
-			func_( handle_script ) { }
+        action() :
+            func_( handle_script ) { }
 
-		template < typename T >
-		action( T &&t ) :
-			func_( t ) { }
+        template < typename T >
+        action( T &&t ) :
+            func_( t ) { }
 
-		void operator()( int argc, char *argv[] )
-		{
-			func_( argc, argv );
-		}
+        void operator()( int argc, char *argv[] )
+        {
+            func_( argc, argv );
+        }
 
-	private:
-		function< void( int, char *[] ) > func_;
+    private:
+        function< void( int, char *[] ) > func_;
 };
 
 map< string, action > initialize_argument_handling();
@@ -47,213 +47,231 @@ map< string, action > initialize_argument_handling();
 static map< string, action > argument_handling( initialize_argument_handling() );
 
 static const string defaultHeader(
-		"#include <array>\n"
-		"#include <bitset>\n"
-		"#include <deque>\n"
-		"#include <forward_list>\n"
-		"#include <list>\n"
-		"#include <map>\n"
-		"#include <queue>\n"
-		"#include <set>\n"
-		"#include <stack>\n"
-		"#include <unordered_map>\n"
-		"#include <unordered_set>\n"
-		"#include <vector>\n"
-		"#include <algorithm>\n"
-		"#include <chrono>\n"
-		"#include <functional>\n"
-		"#include <iterator>\n"
-		"#include <memory>\n"
-		"#include <stdexcept>\n"
-		"#include <tuple>\n"
-		"#include <utility>\n"
-		"#include <locale>\n"
-		"#include <codecvt>\n"
-		"#include <string>\n"
-		"#include <regex>\n"
-		"#include <fstream>\n"
-		"#include <iomanip>\n"
-		"#include <ios>\n"
-		"#include <iosfwd>\n"
-		"#include <iostream>\n"
-		"#include <istream>\n"
-		"#include <ostream>\n"
-		"#include <sstream>\n"
-		"#include <streambuf>\n"
-		"#include <exception>\n"
-		"#include <limits>\n"
-		"#include <new>\n"
-		"#include <typeinfo>\n"
-		"#include <thread>\n"
-		"#include <mutex>\n"
-		"#include <condition_variable>\n"
-		"#include <future>\n"
-		"#include <complex>\n"
-		"#include <random>\n"
-		"#include <valarray>\n"
-		"#include <numeric>\n"
-		"#include <commandline_utilities>\n"
-		"using namespace std;\n"
-		"using namespace filesystem;\n"
-		"int main( int argc, const char *argv_c_str[] ) {\n"
-		"\tstd::map< int, string > argv;\n"
-		"\tfor ( int i = 0; i < argc; ++i ) argv[ i ] = argv_c_str[ i ];\n" );
+        "#include <array>\n"
+        "#include <bitset>\n"
+        "#include <deque>\n"
+        "#include <forward_list>\n"
+        "#include <list>\n"
+        "#include <map>\n"
+        "#include <queue>\n"
+        "#include <set>\n"
+        "#include <stack>\n"
+        "#include <unordered_map>\n"
+        "#include <unordered_set>\n"
+        "#include <vector>\n"
+        "#include <algorithm>\n"
+        "#include <chrono>\n"
+        "#include <functional>\n"
+        "#include <iterator>\n"
+        "#include <memory>\n"
+        "#include <stdexcept>\n"
+        "#include <tuple>\n"
+        "#include <utility>\n"
+        "#include <locale>\n"
+        "#include <codecvt>\n"
+        "#include <string>\n"
+        "#include <regex>\n"
+        "#include <fstream>\n"
+        "#include <iomanip>\n"
+        "#include <ios>\n"
+        "#include <iosfwd>\n"
+        "#include <iostream>\n"
+        "#include <istream>\n"
+        "#include <ostream>\n"
+        "#include <sstream>\n"
+        "#include <streambuf>\n"
+        "#include <exception>\n"
+        "#include <limits>\n"
+        "#include <new>\n"
+        "#include <typeinfo>\n"
+        "#include <thread>\n"
+        "#include <mutex>\n"
+        "#include <condition_variable>\n"
+        "#include <future>\n"
+        "#include <complex>\n"
+        "#include <random>\n"
+        "#include <valarray>\n"
+        "#include <numeric>\n"
+        "#include <commandline_utilities>\n"
+        "using namespace std;\n"
+        "using namespace filesystem;\n"
+        "int main( int argc, const char *argv_c_str[] ) {\n"
+        "\tstd::map< int, string > argv;\n"
+        "\tfor ( int i = 0; i < argc; ++i ) argv[ i ] = argv_c_str[ i ];\n" );
 
 Config get_config()
 {
-	const char *homeDir = getenv( "HOME" );
-	if ( !homeDir ) throw logic_error( "environment variable 'HOME' not set!" );
+    const char *homeDir = getenv( "HOME" );
+    if ( !homeDir ) throw logic_error( "environment variable 'HOME' not set!" );
 
-	return Config( Config::readConfigFile( path( homeDir ) / ".nativescript" / "config" ) );
+    return Config( Config::readConfigFile( path( homeDir ) / ".nativescript" / "config" ) );
 }
 
-const path executable_path( const Config &config, const path &script, build_type build = release )
+path executable_path( const Config &config, const path &script, build_type build = release )
 {
-	path exe = config.buildDirectory() / script;
-	if ( build == debug )
-	{
-		exe += "d";
-	}
-	return exe;
+    path exe = config.buildDirectory() / script;
+    if ( build == debug )
+    {
+        exe += "d";
+    }
+    return exe;
+}
+
+path debug_script_path( const Config &config, const path &script )
+{
+    return config.buildDirectory() / script + ".debugger";
 }
 
 const path get_executable( const Config &config, const path &script, build_type build = release )
 {
-	const path exePath = executable_path( config, script, build );
-	const path sourcePath = config.sourceDirectory() / script + ".cpp";
+    const path exePath = executable_path( config, script, build );
+    const path sourcePath = config.sourceDirectory() / script + ".cpp";
 
-	if ( !exists( exePath ) || modification_date( sourcePath ) <= modification_date( path( script ) ) )
-	{
-		const path scriptDir = path( script ).dirname();
-		make_directory( config.sourceDirectory() / scriptDir );
+    if ( !exists( exePath ) || modification_date( sourcePath ) <= modification_date( path( script ) ) )
+    {
+        const path scriptDir = path( script ).dirname();
+        make_directory( config.sourceDirectory() / scriptDir );
 
-		ofstream source( sourcePath.c_str() );
+        ofstream source( sourcePath.c_str() );
 
-		ifstream scriptsource( script.c_str() );
-		string line;
-		getline( scriptsource, line );
-		if ( line.substr( 0, 2 ) != "#!" )
-		{
-			throw logic_error( "hashbang not found!" );
-		}
+        ifstream scriptsource( script.c_str() );
+        string line;
+        getline( scriptsource, line );
+        if ( line.substr( 0, 2 ) != "#!" )
+        {
+            throw logic_error( "hashbang not found!" );
+        }
 
-		ifstream header( config.header().c_str() );
-		if ( header )
-		{
-			source << header.rdbuf();
-		}
-		else
-		{
-			source << defaultHeader;
-		}
+        ifstream header( config.header().c_str() );
+        if ( header )
+        {
+            source << header.rdbuf();
+        }
+        else
+        {
+            source << defaultHeader;
+        }
 
-		while ( getline( scriptsource, line ) )
-		{
-			source << line << endl;
-		}
+        while ( getline( scriptsource, line ) )
+        {
+            source << line << endl;
+        }
 
-		source << "}";
+        source << "}";
 
-		source.close();
+        source.close();
 
-		make_directory( config.buildDirectory() / scriptDir );
+        make_directory( config.buildDirectory() / scriptDir );
 
-		stringstream command;
-		command << config.compilerCommand() << ' '
-				<< config.cxxFlags() << ' '
-				<< ( ( build == release ) ? config.releaseFlags() : config.debugFlags() ) << ' '
-				<< config.includeDirectories() << ' '
-				<< config.linkDirectories() << ' '
-				<< config.libraries() << ' '
-				<< "-I" << config.baseDirectory() / "include" << ' '
-				<< "-L" << config.baseDirectory() / "lib" << ' '
-				<< sourcePath << ' '
-				<< "-lcommandline_utilities "
-				<< "-o " << exePath;
+        stringstream command;
+        command << config.compilerCommand() << ' '
+                << config.cxxFlags() << ' '
+                << ( ( build == release ) ? config.releaseFlags() : config.debugFlags() ) << ' '
+                << config.includeDirectories() << ' '
+                << config.linkDirectories() << ' '
+                << config.libraries() << ' '
+                << "-I" << config.baseDirectory() / "include" << ' '
+                << "-L" << config.baseDirectory() / "lib" << ' '
+                << sourcePath << ' '
+                << "-lcommandline_utilities "
+                << "-o " << exePath;
 
-		cout << command.str() << endl;
-		if ( system( command.str().c_str() ) )
-		{
-			throw logic_error( "problem compiling script" );
-		}
-	}
+        cout << command.str() << endl;
+        if ( system( command.str().c_str() ) )
+        {
+            throw logic_error( "problem compiling script" );
+        }
+    }
 
-	return exePath;
+    return exePath;
 }
 
 void handle_script( int argc, char *argv[] )
 {
-	const path script( absolute( path( argv[ 1 ] ), cwd() ) );
+    const path script( absolute( path( argv[ 1 ] ), cwd() ) );
 
-	Config config( get_config() );
+    Config config( get_config() );
 
-	const path exePath = get_executable( config, script );
+    const path exePath = get_executable( config, script );
 
-	execv( exePath.c_str(), argv + 1 );
+    execv( exePath.c_str(), argv + 1 );
 }
 
 void debug_script( int argc, char *argv[] )
 {
-	const path script( absolute( path( argv[ 2 ] ), cwd() ) );
+    const path script( absolute( path( argv[ 2 ] ), cwd() ) );
 
-	Config config( get_config() );
+    Config config( get_config() );
 
-	const string exe( get_executable( config, script, debug ).string() );
-//	argv[ 1 ] = const_cast< char* >( exe.c_str() );
+    const string exe( get_executable( config, script, debug ).string() );
 
-	// for some reason, lldb won't start in execv
-	exit( system( ( config.debugCommand() + " " + exe ).c_str() ) );
+    const string debug_script( debug_script_path( config, script ).string() );
+
+    ofstream stream( debug_script.c_str() );
+
+    stream << "file " << exe << endl
+                 << "r ";
+    for ( int i = 3; i < argc; ++i )
+    {
+        stream << argv[ i ] << ' ';
+    }
+    stream << endl << "q" << endl;
+
+    stream.close();
+
+    // for some reason, lldb won't start in execv
+    exit( system( ( config.debugCommand() + " -s " + debug_script ).c_str() ) );
 //	execv( config.debugCommand().c_str(), argv + 1 );
 }
 
 void print_usage( int argc, char *argv[] )
 {
-	cout << "you're doing it wrong!" << endl;
+    cout << "you're doing it wrong!" << endl;
 }
 
 void show_executable( int argc, char *argv[] )
 {
-	const path script( absolute( path( argv[ 2 ] ), cwd() ) );
-	Config config( get_config() );
-	cout << executable_path( config, script ) << endl;
+    const path script( absolute( path( argv[ 2 ] ), cwd() ) );
+    Config config( get_config() );
+    cout << executable_path( config, script ) << endl;
 }
 
 void show_debug_executable( int argc, char *argv[] )
 {
-	const path script( absolute( path( argv[ 2 ] ), cwd() ) );
-	Config config( get_config() );
-	cout << executable_path( config, script, debug ) << endl;
+    const path script( absolute( path( argv[ 2 ] ), cwd() ) );
+    Config config( get_config() );
+    cout << executable_path( config, script, debug ) << endl;
 }
 
 map< string, action > initialize_argument_handling()
 {
-	map< string, action > argument_handling;
-	argument_handling[ "--debug" ] = debug_script;
-	argument_handling[ "--usage" ] =
-			argument_handling[ "--help" ] = print_usage;
-	argument_handling[ "--exe" ] = show_executable;
-	argument_handling[ "--debug_exe" ] = show_debug_executable;
-	return argument_handling;
+    map< string, action > argument_handling;
+    argument_handling[ "--debug" ] = debug_script;
+    argument_handling[ "--usage" ] =
+            argument_handling[ "--help" ] = print_usage;
+    argument_handling[ "--exe" ] = show_executable;
+    argument_handling[ "--debug_exe" ] = show_debug_executable;
+    return argument_handling;
 }
 
 int main( int argc, char *argv[] )
 {
-	try
-	{
-		if ( argc < 2 )
-		{
-			print_usage( argc, argv );
-		}
-		else
-		{
-			argument_handling[ argv[ 1 ] ]( argc, argv );
-		}
-	}
-	catch ( const exception &err )
-	{
-		cerr << err.what() << endl;
-		return 1;
-	}
+    try
+    {
+        if ( argc < 2 )
+        {
+            print_usage( argc, argv );
+        }
+        else
+        {
+            argument_handling[ argv[ 1 ] ]( argc, argv );
+        }
+    }
+    catch ( const exception &err )
+    {
+        cerr << err.what() << endl;
+        return 1;
+    }
 
-	return  0;
+    return  0;
 }
